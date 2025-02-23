595555bcfc9edea38322c85df2ea48c420590cba
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-02-23 13:35:26 +0600

    add @nestjs/config and @nestjs/swagger dependencies; configure Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Documentation Setup
+  const config = new DocumentBuilder()
+    .setTitle('HLS-VIDEO')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT, () => {
+    console.log('// --------------------------------------------------');
+    logger.log(`Server URL        : http://localhost:${process.env.PORT}`);
+    logger.log(`API Documentation : http://localhost:${process.env.PORT}/api`);
+    console.log('// --------------------------------------------------');
+  });
 }
 bootstrap();
